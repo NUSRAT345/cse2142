0f300145915fd8c98bff88764530518b3268e162
NUSRAT345 <nusratoishe345@gmail.com>
2022-11-30 04:17:51 +0600

    step5

--- StudentList.java
+++ StudentList.java
@@ -5,11 +5,13 @@ public class StudentList {
 	public static String students;
 	public static String names[];
 
+	public static Constant constant = new Constant();
+
 	public static void read() {
 		try {
-			BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("students.txt")));
+			BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(constant.FileNmae)));
 			students = reader.readLine();
-			names = students.split(", ");
+			names = students.split(constant.split);
 		} catch (Exception e) {
 
 		}
@@ -17,7 +19,7 @@ public class StudentList {
 
 	public static void write(String update) {
 		try {
-			BufferedWriter write = new BufferedWriter(new FileWriter("students.txt", false));
+			BufferedWriter write = new BufferedWriter(new FileWriter(constant.FileNmae, false));
 			write.flush();
 			write.write(update);
 			write.close();
@@ -30,50 +32,50 @@ public class StudentList {
 
 //		Check arguments
 		if(args.length == 0){
-			System.out.println("Invalid");
+			System.out.println(constant.Error);
 		}
-		else if(args[0].equals("a")) {
-			System.out.println("Loading data ...");
+		else if(args[0].equals(constant.showNames)) {
+			System.out.println(constant.dataLoad);
 			read();
 			for(String name : names) {
 				System.out.println(name);
 			}
-			System.out.println("Data Loaded.");
+			System.out.println(constant.dataLoaded);
 		}
-		else if(args[0].equals("r")) {
-			System.out.println("Loading data ...");
+		else if(args[0].equals(constant.randomName)) {
+			System.out.println(constant.dataLoad);
 			read();
 			Random random = new Random();
 			int index = random.nextInt(names.length);
 			System.out.println(names[index]);
-			System.out.println("Data Loaded.");
+			System.out.println(constant.dataLoaded);
 		}
-		else if(args[0].contains("+")) {
-			System.out.println("Loading data ...");
+		else if(args[0].contains(constant.addName)) {
+			System.out.println(constant.dataLoad);
 			read();
 			String newname = args[0].substring(1);
 			Date date = new Date();
-			String dateformate = "dd/mm/yyyy-hh:mm:ss a";
+			String dateformate = constant.dateFormat;
 			DateFormat dateFormat = new SimpleDateFormat(dateformate);
 			String finaldate = dateFormat.format(date);
-			write(students + ", " + newname + "\nList last updated on " + finaldate);
-			System.out.println("Data Loaded.");	
+			write(students + constant.split + newname + constant.lastUpdate + finaldate);
+			System.out.println(constant.dataLoaded);
 		}
-		else if(args[0].contains("?")) {
-			System.out.println("Loading data ...");
+		else if(args[0].contains(constant.query)) {
+			System.out.println(constant.dataLoad);
 			read();
 			boolean done = false;
 			String newname = args[0].substring(1);
 			for(int idx = 0; idx<names.length && !done; idx++) {
 				if(names[idx].equals(newname)) {
-					System.out.println("We found it!");
+					System.out.println(constant.Found);
 						done=true;
 				}
 			}
-			System.out.println("Data Loaded.");				
+			System.out.println(constant.dataLoaded);
 		}
-		else if(args[0].contains("c")) {
-			System.out.println("Loading data ...");
+		else if(args[0].contains(constant.countWords)) {
+			System.out.println(constant.dataLoad);
 			read();
 			char characters[] = students.toCharArray();
 			int count = 1;
@@ -82,8 +84,8 @@ public class StudentList {
 					count++;
 				}
 			}
-			System.out.println(count +" word(s) found " );
-			System.out.println("Data Loaded.");				
+			System.out.println(count + constant.wordsFound );
+			System.out.println(constant.dataLoaded);
 		}
 	}
 }
